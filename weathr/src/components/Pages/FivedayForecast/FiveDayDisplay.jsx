ad96c6602125afff46a17793afce0a12f9cc49ea
Abdul Ahmad <abdulfatah.a.ahmad@gmx.com>
2019-07-17 12:41:26 -0400

    Completed MVP

---
+++ weathr/src/components/Pages/FivedayForecast/FiveDayDisplay.jsx
@@ -0,0 +1,29 @@
+    
+import React from 'react';
+import FiveDayForecast from  './FiveDayForecast.jsx'
+
+const FiveDayDisplay = props =>{
+
+    console.log(props);
+
+    return(
+
+        <ul className='character-list'>
+        {props.fivedayforecast.map( day => {
+
+     return  ( <FiveDayDisplay 
+        
+            key = {day}
+            temp = {day}
+
+        /> 
+ ) } )
+        
+        
+        }
+
+        </ul>
+    );
+}
+
+export default FiveDayDisplay;
