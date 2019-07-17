330f1c2fe9f1959974f3817b0a39c6f7b8d7a011
Abdul Ahmad <abdulfatah.a.ahmad@gmx.com>
2019-07-16 22:20:37 -0400

    Added file structure to the React app

---
+++ weathr/src/BaseRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CurrentTemp from './components/Pages/CurrentTemp';
+import FiveDayForecast from './components/Pages/FiveDayForecast'
+
+
+function App() {
+  return (
+ <>
+       
+        <CurrentTemp />
+        <FiveDayForecast />
+     
+     </>
+  );
+}
+
+export default App;
---
+++ weathr/src/components/Pages/FiveDayForecast.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class FiveDayForecast  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <>
+            <h1>Five days forecast</h1>
+            </>
+        );
+    }
+}
+ 
+export default FiveDayForecast;
